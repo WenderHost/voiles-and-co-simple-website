399d5b747c2d315eb97f2ccb0b55433c01d5d86b
Michael Wender <michael@michaelwender.com>
2026-02-17 16:50:39 -0500

    Refactoring file setup

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'smtp2go_api_key' => '_SMTP2GO_API_KEY_',
+  'contact_to' => 'example@example.com',
+  'from_email' => 'example@example.com',
+  'from_name' => 'Voiles & Co.',
+  'turnstile_site_keyAA' => '_TURNSTILE_SITE_KEY_',
+  'turnstile_secret' => '_TURNSTILE_SECRET_KEY_',
+  'site_name' => 'Voiles & Co.',
+  'site_url' => 'https://voilesandco.com/'
+];
